c7bec506a5b6da09fd247effdd776e2326b6659f
yasermoamd <yasermoamd@hotmail.com>
2024-03-19 13:24:11 +0000

    user model for manage regisration/login

---
+++ models/user.py
@@ -0,0 +1,10 @@
+"""
+        user credential - manage user login to public/dashboard
+        @login: 
+        @register:
+        @logout:
+        @reset_password:
+"""
+
+class User:
+        pass
